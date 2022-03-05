67e82742e1326e308e9d17f0ca4cac8d0a7e76f3
mike whitfield <mwhitfieldwork@gmail.com>
2022-03-05 14:23:44 -0600

    Add comments around the second array subtracting from the frequecny of the object

--- anagram/anagram_1.js
+++ anagram/anagram_1.js
@@ -14,12 +14,19 @@ function anagram(str1,str2){
  for(let i = 0; i<strArr1.length; i++){
 	let letter = strArr1[i];
 	freq[letter] ? freq[letter]++ : freq[letter] =1; //check if strArr[i] 
-	                                                 //if its in the string array add 1
+	                                                 //if it isnt in the string array add 1
 													 //if its in there more than once increment the frequency
  }
  
  for(let j=0; j<strArr2.length; j++){
 	let char = strArr2[j];
+	// loop through the second array 
+	// if you dont find the letter of the second array in the freq obj
+	// then exit function.
+	//if you do find it in the freq obj then subtract the frequency value 
+	// from the letter it found in  the object
+	// if all of the letters from the second array are found in the obj
+	// then the function returns true
 	if(!freq[char]){
 	 return false;
 	}
